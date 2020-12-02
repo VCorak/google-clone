6512af3ff85fc3590421e392c651f69c3f5c6629
VCorak <68239365+VCorak@users.noreply.github.com>
2020-12-02 13:26:14 +0100

    Updated Search.js

--- src/Search.js
+++ src/Search.js
@@ -5,7 +5,7 @@ import MicIcon from "@material-ui/icons/Mic";
 import { Button } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
-import { actionTypes } from "./reducer.js";
+import { actionTypes } from "./reducer";
 
 function Search({ hideButtons = false }) {
   const [{}, dispatch] = useStateValue(); //hook from reducer.js, shoot action to data layer, so we can change it
@@ -20,7 +20,7 @@ function Search({ hideButtons = false }) {
 
     dispatch({
       type: actionTypes.SET_SEARCH_TERM,
-      term: input,
+      term: input
     });
 
     //do something with input... come back and fix
