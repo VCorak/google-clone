0035f37be5b5f826bb79724ce3a4f3ecaa73f3cf
VCorak <68239365+VCorak@users.noreply.github.com>
2020-12-02 14:41:02 +0100

    Updated hook

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -14,7 +14,7 @@ import RoomIcon from "@material-ui/icons/Room";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 function SearchPage() {
-  const [{ term = "tesla" }, dispatch] = useStateValue();
+  const [{ term = "tesla" }] = useStateValue();
 
   //LIVE API CALL
   const { data } = useGoogleSearch(term);
